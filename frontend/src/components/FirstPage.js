5b2ed8ec6652e79794ad0a18d50c6667b6361722
john hansson <johnhansson90@gmail.com>
2021-04-06 15:25:37 +0200

    styling frontpage

--- frontend/src/components/FirstPage.js
+++ frontend/src/components/FirstPage.js
@@ -1,10 +1,18 @@
 import { Link } from 'react-router-dom';
+import styles from '../styles/FirstPage.module.css';
+import airbeanlogo from '../assets/svg/airbean-landing.svg'
+import leavesleft from '../assets/svg/intro-graphic-left.svg'
+import leavesright from '../assets/svg/intro-graphic-right.svg'
 
 const FirstPage = () => {
   return (
-    <Link to="/menu">
-    <h1>Hello from LandingPage</h1>
-    </Link>
+    <Link to='/menu'>
+    <section className={styles.grid}>
+        <img src={leavesleft} alt='Left side leaves' className={styles.gridItemLeft} />
+        <img src={airbeanlogo} alt='Airbean logotype' className={styles.gridItem} />
+        <img src={leavesright} alt='Right side leaves' className={styles.gridItemRight} />
+    </section>
+</Link>
   )
 }
 
--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,33 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import styles from "../styles/Menu.module.css";
 
-  const Menu = () =>  {
-    //set useState "initial state to an empty array"
-    const [data, setData] = useState([]);
-    //save backend URL
-    const apiUrl = 'http://localhost:8080/api/menu';
-    //useEffect updates the object
-    useEffect(() => {
-      fetch(apiUrl)
+
+
+const Menu = () => {
+  //set useState "initial state to an empty array"
+  const [data, setData] = useState([]);
+  //save backend URL
+  const apiUrl = "http://localhost:8080/api/menu";
+  //useEffect updates the object
+  useEffect(() => {
+    fetch(apiUrl)
       .then((response) => response.json())
-      .then(data => setData(data.beans));
-      
-    }, [])
-  
-    return (
-        <div>
-          <ul>
-          {data.map((element) => { //mapping saved API in data and render it with the argument with title frrom json
-            return (
+      .then((data) => setData(data.beans));
+  }, []);
+
+
+  return (
+    <section className={styles.menu}>
+      <h1>Meny</h1>
+      <ul className={styles.orders}>
+        {data.map((element) => {
+          //mapping saved API in data and render it with the argument with title from json
+          return (
             <li>{element.title}</li>
-            )
-          })}
-          </ul>
-         
-        </div>
-        
-    )
-  
-}
+          )
+
+        })}
+      </ul>
+    </section>
+  );
+};
 export default Menu;
-
-
