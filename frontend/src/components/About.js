830d47cf43e860454c9e6e724ee1ba4c06237965
john hansson <johnhansson90@gmail.com>
2021-06-18 09:53:03 +0200

    css page about done

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,27 +1,40 @@
-import React from 'react'
-import styles from '../styles/About.module.css';
-import VD from '../assets/img/vd.png';
+import React from "react";
+import styles from "../styles/About.module.css";
+import VD from "../assets/img/vd.png";
 import Footer from "./Footer";
 import Header from "./Header";
-import Nav from '../components/Nav.js';
+import Nav from "../components/Nav.js";
 
 const About = () => {
-    return (
-        <section className={styles.about}>
-            <Header />
-            <Nav />
-            <h1>Vårt kaffe</h1>
-            <p>Pumpkin spice mug, barista cup, sit macchiato, kopi-luwak, doppio, grounds dripper, crema, strong whipped, variety extra iced id lungo half and half mazagran. Pumpkin spice.
-            Que dark fair trade, spoon decaffeinated, barista wings whipped, as rich aftertaste, con panna milk black, arabica white rich beans single shot extra affogato. So affogato macchiato sit extraction instant grinder seasonal organic, turkish single shot, single origin, and robusta strong to go so dripper. Viennese froth, grounds caramelization skinny aromatic cup kopi-luwak, fair trade flavour, frappuccino medium, café au lait flavour cultivar ut bar instant kopi-luwak.
-            Roast id macchiato, single shot siphon mazagran milk fair trade est aroma a half and half and, so, galão iced to go, whipped as cream cup pumpkin spice iced. At extra, rich grinder, brewed to go, steamed half and half at, that, percolator macchiato trifecta and body as arabica dripper. In galão black java milk sit trifecta, robusta, acerbic café au lait instant shop latte. Seasonal bar shop filter aroma id, crema, affogato viennese cultivar aftertaste, seasonal, percolator cream black, galão flavour, milk aromatic turkish skinny crema.
-            </p>
-            
-            <img src={VD} alt='vd img' className={styles.woman} />
-            <h2>Eva Cortado</h2>
-            <p>VD & Grundare</p>
-            <Footer />
-        </section>
-    );
-}
+  return (
+    <section>
+      <Header />
+      <Nav />
+      <section className={styles.container}>
+
+      <section className={styles.about}>
+        <article>
+          <h1>Vårt kaffe</h1>
+          <p className={styles.firstColumn}>
+            Pumpkin spice mug, barista cup, sit macchiato, kopi-luwak, doppio,
+            grounds dripper, crema, strong whipped, variety extra iced id lungo
+            half and half mazagran. Pumpkin spice.
+          </p>
+          <p className={styles.secondColumn}>
+          Que dark fair trade, spoon decaffeinated, barista wings whipped, as rich aftertaste, con panna milk black, arabica white rich beans single shot extra affogato. So affogato macchiato sit extraction instant grinder seasonal organic, turkish single shot, single origin, and robusta strong to go so dripper. Viennese froth, grounds caramelization skinny aromatic cup kopi-luwak, fair trade flavour, frappuccino medium, café au lait flavour cultivar ut bar instant kopi-luwak. <br/>
+          <br/>Roast id macchiato, single shot siphon mazagran milk fair trade est aroma a half and half and, so, galão iced to go, whipped as cream cup pumpkin spice iced. At extra, rich grinder, brewed to go, steamed half and half at, that, percolator macchiato trifecta and body as arabica dripper. In galão black java milk sit trifecta, robusta, acerbic café au lait instant shop latte. Seasonal bar shop filter aroma id, crema, affogato viennese cultivar aftertaste, seasonal, percolator cream black, galão flavour, milk aromatic turkish skinny crema.
+          </p>
+        </article>
+      </section>
+    <section className={styles.imgInfo}>
+      <img src={VD} alt="vd img" className={styles.woman} />
+      <h2>Eva Cortado</h2>
+      <p>VD & Grundare</p>
+      </section>
+      <Footer />
+      </section>
+    </section>
+  );
+};
 
 export default About;
